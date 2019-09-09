7285b889e5ae5c14b822093fb833d58747cd4719
John Paul Helveston <john.helveston@gmail.com>
2019-09-09 08:53:33 -0400

    Updated to just install all the packages directly

--- run.R
+++ run.R
@@ -1,5 +1,13 @@
-install.packages('devtools', repos = 'https://cloud.r-project.org')
-library(devtools)
-install_github('jhelvy/jhelvyr')
-library(jhelvyr)
-installAllThePackages()
+# Install a bunch of packages that I frequently use
+packageList = read.csv(
+    file = 'https://raw.github.com/jhelvy/jhelvyr/master/data/packageList.csv',
+    header = FALSE)
+install.packages(
+    as.character(packageList[,1]),
+    repos        = 'https://cloud.r-project.org/',
+    dependencies = T)
+
+# Install other packages from github
+library('devtools')
+install_github('jhelvy/logitr', force = T)
+install_github('jhelvy/jhelvyr', force = T)
